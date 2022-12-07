1d82d48cbb984c910c2f75109208067c9ca77c12
herlok <xdherlokxd@gmail.com>
2022-12-07 13:04:33 +0100

    js updated

--- js/js.js
+++ js/js.js
@@ -1,3 +1,7 @@
-var deckSize = document.getElementById('deckSize');
-var selectValue = deckSize.value; 
-console.log(selectValue);
+function cardGenerate()
+{
+    var deckSize = document.querySelector('.deckSize');
+    var selectValue = deckSize.value; 
+    console.log(selectValue);
+    console.log(deckSize);
+}
