9de213da300e341d751653bdb6082255e7c2a331
herlok <xdherlokxd@gmail.com>
2022-12-08 15:00:00 +0100

    generates cards well

--- js/js.js
+++ js/js.js
@@ -2,6 +2,15 @@ function cardGenerate()
 {
     var deckSize = document.querySelector('.deckSize');
     var selectValue = deckSize.value; 
-    console.log(selectValue);
-    console.log(deckSize);
+    let container = document.querySelector('.cards');
+
+    container.replaceChildren(); 
+
+    for (let i = 1; i <= selectValue; i++) 
+    {
+            let card = document.createElement("li");
+            card.className = "card";
+            card.innerHTML = "<div class='view back_view'>" + "<img src='img/img-1.png'>" + "</div>" + "<div class='view front_view'>" + "<span class='material-icons'>question_mark</span>" + "</div>";            
+            container.appendChild(card);
+    }
 }
