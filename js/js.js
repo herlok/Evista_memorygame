d78df522cf562c87486cf165bb033e283dffc153
herlok <xdherlokxd@gmail.com>
2022-12-08 16:43:58 +0100

    flip works kinda

--- js/js.js
+++ js/js.js
@@ -8,9 +8,42 @@ function cardGenerate()
 
     for (let i = 1; i <= selectValue; i++) 
     {
-            let card = document.createElement("li");
-            card.className = "card";
-            card.innerHTML = "<div class='view back_view'>" + "<img src='img/img-1.png'>" + "</div>" + "<div class='view front_view'>" + "<span class='material-icons'>question_mark</span>" + "</div>";            
-            container.appendChild(card);
+        let card = document.createElement("li");
+        card.className = "card";
+        card.innerHTML = "<div class='view back_view'>" + "<img src='img/img-" + i + ".png'>" + "</div>" + "<div class='view front_view'>" + "<span class='material-icons'>question_mark</span>" + "</div>";            
+        container.appendChild(card);
+
+        let back_view = card.getElementsByTagName('div')[0];
+        let front_view = card.getElementsByTagName('div')[1];
+        
+        card.addEventListener('click', (e) => {
+            if(card.value=="back_view")
+            {
+                back_view.style.display = "none";
+                console.log('back click volt');
+            }
+            else
+            {
+                console.log(front_view);
+                front_view.style.display = "none";
+                console.log('front click volt');
+            }
+        })
+        
+
+
+        /*let back_view = document.createElement("div");
+        back_view.innerHTML = 'back';
+        back_view.addEventListener('click', (e) => {
+            console.log('back view click volt');
+        })
+        container.appendChild(back_view);
+        
+        let front_view = document.createElement("div");
+        front_view.innerHTML = 'front';
+        front_view.addEventListener('click', (e) => {
+            console.log('front_view  click volt');
+        })
+        container.appendChild(front_view);*/
     }
 }
